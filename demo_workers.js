e3ca40f705e3e3ea6e19fac6411548f65bc6cb3e
unknown <ash.ishu025@gmail.com>
2017-09-19 15:03:42 +0530

    clean commit

--- demo_workers.js
+++ demo_workers.js
@@ -1,45 +1,8 @@
-//https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js;
+
 function doSomething() {  
-  //postMessage( test);
-
-  // $.ajax({
-  //      url: 'package.json',
-  //      dataType: 'json',
-  //      success: function(data) {
-          
-  //         debugger;
-  //         postMessage(data);
-
-  //      },
-  //     statusCode: {
-  //        404: function() {
-  //          alert('There was a problem with the server.  Try again soon!');
-  //        }
-  //      }
-  //   });
-
   ajax('package.json',null, null,null);
 }
 setTimeout ( "doSomething()", 1000 );
-// var test = {
-//   "name": "webworker",
-//   "version": "1.0.0",
-//   "description": "will be building a demo app to see WebWorker working",
-//   "main": "index.html",
-//   "scripts": {
-//     "test": "echo \"Error: no test specified\" && exit 1"
-//   },
-//   "repository": {
-//     "type": "git",
-//     "url": "git+https://github.com/MishraAshish/WebWorker.git"
-//   },
-//   "author": "",
-//   "license": "ISC",
-//   "bugs": {
-//     "url": "https://github.com/MishraAshish/WebWorker/issues"
-//   },
-//   "homepage": "https://github.com/MishraAshish/WebWorker#readme"
-// }
 var ajax = function (url, data, callback, type) {
   var data_array, data_string, idx, req, value;
   if (data == null) {
@@ -61,11 +24,9 @@ var ajax = function (url, data, callback, type) {
   req = new XMLHttpRequest();
   req.open(type, url, false);
   req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  req.onreadystatechange = function () {
-    debugger;
+  req.onreadystatechange = function () {    
     if (req.readyState === 4 && req.status === 200) {
-      postMessage(req.responseText);
-      debugger;
+      postMessage(req.responseText);      
       return callback(req.responseText);
     }
   };
